d3a5a5a06d78744d63ed81e3220d46394b1fac21
angcyo <angcyo@126.com>
2015-09-01 21:30:26 +0800

    添加登录界面

---
+++ app/src/main/java/com/angcyo/oaschool/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.angcyo.oaschool;
+
+/**
+ * Created by angcyo on 15-09-01-001.
+ */
+public class WelcomeActivity extends BaseActivity {
+    @Override
+    protected void initView() {
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void initAfter() {
+
+    }
+}
